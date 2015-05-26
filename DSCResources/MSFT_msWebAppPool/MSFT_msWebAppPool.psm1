975b10a695f74d7b4b8018282546a4fd55dbea68
Nico Engelen <engelen.nico@googlemail.com>
2015-05-26 11:52:04 +0100

    Adding functionality to run AppPool under different Identity

--- DSCResources/MSFT_msWebAppPool/MSFT_msWebAppPool.psm1
+++ DSCResources/MSFT_msWebAppPool/MSFT_msWebAppPool.psm1
@@ -24,14 +24,21 @@ function Get-TargetResource
 
     if($AppPool -ne $null)
     {
-        $Ensure = "Present"
-        $State  = $AppPool.state
+        $Ensure       = "Present"
+        $State        = $AppPool.state
+        $IdentityType = $AppPool.processModel.identityType
+        If ($AppPool.processModel.Username -and $AppPool.processModel.Password)
+        {
+           $Cred = New-Object System.Management.Automation.PSCredential($AppPool.processModel.Username,(ConvertTo-SecureString -AsPlainText -Force -String $AppPool.processModel.Password))
+        }
     }
 
     $returnValue = @{
-        Name   = $Name
-        Ensure = $Ensure
-        State  = $State
+        Name         = $Name
+        Ensure       = $Ensure
+        State        = $State
+        IdentityType = $IdentityType
+        Credential   = $Cred
     }
 
     return $returnValue
@@ -53,7 +60,18 @@ function Set-TargetResource
 
         [ValidateSet("Started","Stopped")]
         [System.String]
-        $State = "Started"
+        $State = "Started",
+
+        [ValidateSet("SpecificUser","ApplicationPoolIdentity")]
+        [System.String]
+        $IdentityType,
+
+        [ValidateScript(
+        {
+            $IdentityType -eq "SpecificUser"
+        })]
+        [System.Management.Automation.PSCredential]
+        $Credential
     )
 
     if($Ensure -eq "Absent")
@@ -75,6 +93,34 @@ function Set-TargetResource
         {
             ExecuteRequiredState -Name $Name -State $State
         }
+        if($IdentityType -and $IdentityType -ne $AppPool.identityType)
+        {
+            Write-Verbose "Setting AppPool IdentityType"
+            $ApplicationPool = Get-Item -Path IIS:\AppPools\* | ? {$_.name -eq $Name}
+            $ApplicationPool.processModel.identityType = $IdentityType
+            $ApplicationPool | Set-Item
+        }
+        if($Credential) 
+        { 
+            if($Credential.Username -ne $AppPool.Credential.Username -or
+                [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credential.Password)) -ne
+                [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AppPool.Credential.Password)))
+            {
+                Write-Verbose "Setting Credential"
+                $ApplicationPool = Get-Item -Path IIS:\AppPools\* | ? {$_.name -eq $Name}
+                $ApplicationPool.processModel.Username = $Credential.Username
+                $ApplicationPool.processModel.Password = [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credential.Password))
+                $ApplicationPool | Set-Item
+            }
+        }
+        else
+        {
+            Write-Verbose "Removing Credential information"
+            $ApplicationPool = Get-Item -Path IIS:\AppPools\* | ? {$_.name -eq $Name}
+            $ApplicationPool.processModel.Username = ""
+            $ApplicationPool.processModel.Password = ""
+            $ApplicationPool | Set-Item
+        }
     }
 }
 
@@ -95,21 +141,44 @@ function Test-TargetResource
 
         [ValidateSet("Started","Stopped")]
         [System.String]
-        $State = "Started"
+        $State = "Started",
+
+        [ValidateSet("SpecificUser","ApplicationPoolIdentity")]
+        [System.String]
+        $IdentityType,
+
+        [ValidateScript(
+        {
+            $IdentityType -eq "SpecificUser"
+        })]
+        [System.Management.Automation.PSCredential]
+        $Credential
     )
     $WebAppPool = Get-TargetResource -Name $Name
 
     if($Ensure -eq "Present")
     {
         if($WebAppPool.Ensure -eq $Ensure -and $WebAppPool.State -eq $state)
+                     
         {
-            return $true
+            if(-not $Credential)
+            {
+                return $true
+            }
+            elseif($Credential.Username -eq $WebAppPool.Credential.Username -and
+                [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credential.Password)) -eq
+                [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($WebAppPool.Credential.Password)))
+            {
+                return $true
+            }
         }
     }
     elseif($WebAppPool.Ensure -eq $Ensure)
     {
         return $true
     }
+
+
 
     return $false
 }
